367b80d081c41dcd676dfe31858a1c34f34646d9
TechHypno <techhypno.com@gmail.com>
2022-11-14 01:35:54 +0100

    added ui reload prompt for addons that require it, added SexyMap profile copying

--- addons/SexyMap.lua
+++ addons/SexyMap.lua
@@ -1,12 +1,19 @@
 local _, TOP = ...
 TOP.RegisterAddonHandler("SexyMap",
     -- SexyMap has a built-in global profile functionality,
-    -- enable it and return true to request a ReloadUI.
+    -- enable it, copy the character profile into it and return true to request a ReloadUI.
     function()
+        local function deepCopy(t)
+            local r = {}
+            for k, v in pairs(t) do
+                r[k] = (type(v) == "table") and deepCopy(v) or v
+            end
+            return r
+        end
         local char = (UnitName("player").."-"..GetRealmName())
         if (type(SexyMap2DB) == 'table') and (SexyMap2DB[char] ~= "global") then
             if not SexyMap2DB.global then
-                SexyMap2DB.global = {}
+                SexyMap2DB.global = deepCopy(SexyMap2DB[char])
             end
             SexyMap2DB[char] = "global"
             return true
--- main.lua
+++ main.lua
@@ -1,6 +1,28 @@
 local addonName, TOP = ...
 _G[addonName] = TOP
 
+--Confirmation Dialog
+StaticPopupDialogs["THEONEPROFILE_POPUPDIALOG"] = {
+    text = "POPUPDIALOG_TITLE",
+    button1 = "Reload",
+    button2 = "Cancel",
+    OnShow = function() end,
+    OnAccept = function() end,
+    EditBoxOnEnterPressed = function() end,
+    hasEditBox = false,
+    timeout = 0,
+    exclusive = true,
+    hideOnEscape = true
+}
+
+function TOP.RequestReload(reloadList)
+    local dialog = StaticPopupDialogs["THEONEPROFILE_POPUPDIALOG"]
+    dialog.text = "TheOneProfile needs to Reload the UI to finalize setting up the following addons: "..table.concat(reloadList, ", ")
+    dialog.OnAccept = function(self) ReloadUI() end
+    StaticPopup_Show("THEONEPROFILE_POPUPDIALOG")
+end
+    
+
 local addonHandlers = {}
 function TOP.RegisterAddonHandler(name, func)
     addonHandlers[name] = (type(func) == 'function') and func
@@ -8,8 +30,15 @@ end
 
 function TOP.SetProfiles()
     local reload = false
+    local reloadList = {}
     for name, handler in pairs(addonHandlers) do
-        reload = reload or handler()
+        if handler() then
+            reload = true
+            tinsert(reloadList, name)
+        end
+    end
+    if reload then
+        TOP.RequestReload(reloadList)
     end
 end
 
